17629eb943db6f58f310a98027b96ff82c4f8439
Tyler Ramsey <tyler.elias.ramsey@gmail.com>
2023-03-17 14:47:13 -0400

    Add missing comma

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 // https://nextjs.org/docs/testing#setting-up-jest-with-the-rust-compiler
-const nextJest = require('next/jest')
+const nextJest = require('next/jest');
 
 const createJestConfig = nextJest({
     // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
